a9551f01d189c0e16e9acf849e9e20e54a3e7630
Gray Starling <skvorec92@mail.ru>
2021-05-03 22:50:00 +0300

    morze beta

--- morze/morze.c
+++ morze/morze.c
@@ -2,15 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* Строка неизвестной длины
-char* input_str;
+
+
 
 int main(void)
 {
     printf( "Enter your text to convert Morze code\n");
 
-    scanf("%s", input_str);
+    char *str = NULL, c;
+    int len = 1;
+    str = (char*) malloc(sizeof(char));
+    while((c = getchar()) != '\n') {
+        str[len - 1] = c;
+        len++;
+        str = (char*) realloc(str, len);
+    }
+    str[len - 1] = '\0';
+    printf("%s (%d symbols)\n", str, len - 1);
+    free(str);
+
 
     return 0;
 }
-*/
