0e9c6d27c9861f5098c093a84406764a51b053a4
Gray Starling <skvorec92@mail.ru>
2021-05-03 20:33:09 +0300

    new

---
+++ morze/morze.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Строка неизвестной длины
+char* input_str;
+
+int main(void)
+{
+    printf( "Enter your text to convert Morze code\n");
+
+    scanf("%s", input_str);
+
+    return 0;
+}
+*/
