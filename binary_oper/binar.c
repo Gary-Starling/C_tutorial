4659a2e445fc17eb5c828aea1e02a92a1c987595
Gary-Starling <skvorec92@mail.ru>
2022-04-26 20:26:44 +0300

    binary

--- binary_oper/binar.c
+++ binary_oper/binar.c
@@ -3,6 +3,11 @@
 
 #define MASK 0x03 //маска 1го и 2го разрядов
 
+#define BYTE_MASK 0xFF
+
+unsigned int color = 0x002a162f;
+unsigned char blue, green, red;
+
 unsigned int dig = 2;
 
 int main(int argc, char const *argv[])
@@ -24,11 +29,32 @@ int main(int argc, char const *argv[])
     printf("dig & MASK = 0x%x\n", dig);
 
     dig = dig ^ (1 << 0); 
-    printf("dig ^ (1 < 0) = 0x%x\n", dig);
+    printf("dig ^ (1 << 0) = 0x%x\n", dig);
         dig = dig ^ (1 << 1); 
-    printf("dig ^ (1 < 1) = 0x%x\n", dig);
+    printf("dig ^ (1 << 1) = 0x%x\n", dig);
         dig = dig ^ (1 << 2); 
-    printf("dig ^ (1 < 2) = 0x%x\n", dig);
+    printf("dig ^ (1 << 2) = 0x%x\n", dig);
+
+    dig = 0x03;
+    if ((dig & MASK) == MASK)
+    {
+        printf("dig == 0x02\n");
+    }
+
+    dig <<= 3;
+    printf("%d\n", dig);
+
+    red = color & BYTE_MASK;
+    printf("color = %x\n", color);
+    printf("red = %x\n", red);
+    
+    green = (color >> 8) & BYTE_MASK;
+    printf("color >> 8 = %x\n", color >> 8);
+    printf("green = %x\n", green);
+
+    blue = (color >> 16) & BYTE_MASK;
+    printf("color >> 16 = %x\n", color >> 16);
+    printf("color = %x\n", blue);
     
     return 0;
 }
