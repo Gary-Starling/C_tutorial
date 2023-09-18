8ed7f20a5ed7cb6a2ae3ef2dd98b1f8f98998d3b
Gary-Starling <skvorec92@mail.ru>
2023-09-18 06:59:11 +0300

    hash table (rm func error)

--- hash_table/hash.c
+++ hash_table/hash.c
@@ -1,65 +1,99 @@
 /******************************************************************************
 Simple hash table
 *******************************************************************************/
-
+#include "hash.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-#define HASH_SIZE 50000000
-//(701) //simple numbers
-
-/* Hash data */
-struct sHashItem
-{
-    size_t key;             // hash key    char * val;
-    char *s;                // C-style string
-    struct sHashItem *next; // Using a linked list to resolve collisions
-};
-
-typedef struct sHashItem Item;
-
-typedef struct
-{
-    Item **cell; // array pointer
-    size_t cnt;  //
-    size_t size; // capacity
-} sHashTable;
-
-/* example
-
-    HashTable.cnt;
-    HashTable.size;
-    HashTable.cell[0].key;
-    HashTable.cell[0].val;
-    HashTable.cell[0].next->key;
-
-    HashTable.cell[1].val;
-
-
-*/
-
-void initHashTable(sHashTable *table);
-void insertItem(sHashTable *table, char *data);
-Item *createItem(sHashTable *table, char *data, size_t key);
-
-void error_alloc_exit(void);
-// sHashItem * createItem(unsigned long key, char * val);
-// void freeItem(sHashTable * table);
-
-/* all table */
-sHashTable HashTable;
-
 int main()
 {
-    printf("Hash table test\n");
+    /* all table */
+    sHashTable HashTable;
+    Item *curr;
+
+    printf("Hash table\n");
 
     initHashTable(&HashTable);
 
+    printf("Input data\n");
+    printf("%d\n", hashFunc("Anna"));
+    printf("%d\n", hashFunc("Igor"));
+    printf("%d\n", hashFunc("rogI"));
+    printf("%d\n", hashFunc("Saladin"));
+    printf("%d\n", hashFunc("Richard"));
+    printf("%d\n", hashFunc("Drahcir")); // inversion richard(collision)
+    printf("%d\n", hashFunc("Darhcir")); // +(collision)
+    printf("\n");
+
+    printf("Insert\n");
+    insertItem(&HashTable, "Anna");
+    insertItem(&HashTable, "Igor");
+    insertItem(&HashTable, "rogI");
+    insertItem(&HashTable, "Saladin");
+    insertItem(&HashTable, "Richard");
+    insertItem(&HashTable, "Richard");
+    insertItem(&HashTable, "Drahcir");
+    insertItem(&HashTable, "Darhcir");
+    printf("\n");
+
+    printf("Search\n");
+    curr = findItemVal(&HashTable, "Anna");
+    printf("value : %s, key[%d]\n", curr->val, curr->key);
+    curr = findItemVal(&HashTable, "Igor");
+    printf("value : %s, key[%d]\n", curr->val, curr->key);
+    curr = findItemVal(&HashTable, "Saladin");
+    printf("value : %s, key[%d]\n", curr->val, curr->key);
+    curr = findItemVal(&HashTable, "Richard");
+    printf("value : %s, key[%d]\n", curr->val, curr->key);
+    curr = findItemVal(&HashTable, "Drahcir");
+    printf("value : %s, key[%d]\n", curr->val, curr->key);
+    curr = findItemVal(&HashTable, "Darhcir");
+    printf("value : %s, key[%d]\n", curr->val, curr->key);
+    printf("\n");
+
+    printf("Delete data\n");
+    if (OK == rmItem(&HashTable, "Drahcir"))
+        printf("delate Drahcir ok\n");
+
+    curr = findItemVal(&HashTable, "Drahcir");
+    if (curr != NULL)
+        printf("value : %s, key[%d]\n", curr->val, curr->key);
+
+    curr = findItemVal(&HashTable, "Richard");
+    printf("value : %s, key[%d]\n", curr->val, curr->key);
+
+    curr = findItemVal(&HashTable, "Darhcir");
+    printf("value : %s, key[%d]\n", curr->val, curr->key);
+
+    printf("Delete data\n");
+    if (OK == rmItem(&HashTable, "Igor"))
+        printf("delate Igor ok\n");
+
+    curr = findItemVal(&HashTable, "rogI");
+    printf("value : %s, key[%d]\n", curr->val, curr->key);
+
+    /* printall */
+    for (unsigned int i = 0; i < HASH_SIZE - 1; i++)
+    {
+        curr = HashTable.cell[i];
+        while (curr != NULL)
+        {
+            printf("value : %s, key[%d]\n", curr->val, curr->key);
+            curr = curr->next;
+        }
+    }
+
+    freeTable(&HashTable);
     printf("OK\n");
 
     return 0;
 }
 
+/**
+ * @brief Init and allocate hash table
+ *
+ * @param table - ptr to table
+ */
 void initHashTable(sHashTable *table)
 {
     table->size = HASH_SIZE;
@@ -73,6 +107,12 @@ void initHashTable(sHashTable *table)
         table->cell[i] = NULL; // cell is emppty
 }
 
+/**
+ * @brief Simple hash function
+ *
+ * @param s - ptr to string(value)
+ * @return int - key or -1(error)
+ */
 int hashFunc(const char *s)
 {
     unsigned long c = 0;
@@ -80,16 +120,23 @@ int hashFunc(const char *s)
     if (s == NULL)
         return -1; // error
 
-    while (*s)
-        c += (int)(*s);
+    for (; *s; s++)
+        c += (unsigned long)(*s);
 
     return (c % HASH_SIZE);
 }
 
-void insertItem(sHashTable *table, char *data)
+/**
+ * @brief Insert value to Hash table
+ *
+ * @param table - ptr to table
+ * @param data - input data(C-string)
+ */
+void insertItem(sHashTable *table, const char *data)
 {
     int key = hashFunc(data);
     Item *item = createItem(table, data, key);
+    Item *curr_item;
 
     if (item == NULL)
         error_alloc_exit();
@@ -100,26 +147,53 @@ void insertItem(sHashTable *table, char *data)
         exit(1);
     }
 
-    /* no collision */
-    if (table->cell[key] == NULL)
+    if (table->cell[key] == NULL) /* no collision */
     {
-        if (table->cnt == table->size)
+        if (table->cnt == table->size) /* no space in table */
         {
             printf("Warning, table is full\n");
             free(item);
             return;
         }
-
-        table->cell[key] = item;
-        table->cnt ++;
+        /* 0K*/
+        table->cell[key] = item; /* new data */
+        table->cnt++;
+        printf("%s added in table\n", data);
     }
     else
     {
-        //while (table->cell[key] != NULL)
+        if (table->cell[key]->key == key) /* value == data */
+        {
+            if (strcmp(table->cell[key]->val, data) == 0) /* nothing to do*/
+            {
+                printf("%s = %s\n", table->cell[key]->val, data);
+                return;
+            }
+
+            else /* collision */
+            {
+                curr_item = table->cell[key];
+
+                while (curr_item->next != NULL)
+                    curr_item = curr_item->next;
+
+                curr_item->next = item; /* new data */
+                table->cnt++;
+                printf("%s added in table(Collision)\n", data);
+            }
+        }
     }
 }
 
-Item *createItem(sHashTable *table, char *data, size_t key)
+/**
+ * @brief Create a Item object
+ *
+ * @param table - ptr to table
+ * @param data - input data(C-string)
+ * @param key - key in array
+ * @return Item* - NULL(error) or ptr(ok)
+ */
+Item *createItem(sHashTable *table, const char *data, const int key)
 {
     Item *new_item = (Item *)malloc(sizeof(Item));
 
@@ -129,20 +203,130 @@ Item *createItem(sHashTable *table, char *data, size_t key)
     if (new_item == NULL)
         error_alloc_exit();
 
-    new_item->s = (char *)malloc(strlen(data) + 1);
+    new_item->val = (char *)malloc(strlen(data) + 1); /* alloc mem for string */
 
-    if (new_item->s == NULL)
+    if (new_item->val == NULL)
         error_alloc_exit();
 
     new_item->key = key;
-    strcpy(new_item->s, data);
+    strcpy(new_item->val, data);
 
     return new_item;
 }
 
+/**
+ * @brief Find item in hash table
+ *
+ * @param table - ptr to table
+ * @param v - finding value(C-string)
+ * @return Item* - NULL(error) or ptr(ok)
+ */
+Item *findItemVal(sHashTable *table, const char *v)
+{
+    int key = hashFunc(v);
+    Item *curr_item = NULL;
+
+    if (key < 0)
+    {
+        printf("no input data\n");
+        return NULL;
+    }
+
+    curr_item = table->cell[key];
+
+    while (curr_item != NULL) /* finding */
+    {
+        if (strcmp(v, curr_item->val) == 0)
+            return curr_item;
+
+        curr_item = curr_item->next;
+    }
+
+    return NULL;
+}
+
+/**
+ * @brief delete item from table
+ *
+ * @param table - ptr to table
+ * @param v - rm value(C-string)
+ * @return int - result OK/ERR
+ */
+int rmItem(sHashTable *table, const char *v)
+{
+    int key = hashFunc(v);
+    Item *curr_item = NULL, *temp = NULL, *prv;
+
+    if (key < 0)
+    {
+        printf("No input vaulue\n");
+        return ERR;
+    }
+
+    curr_item = table->cell[key];
+
+    if (curr_item == NULL) /* empty */
+    {
+        printf("value : %s not in table\n", v);
+        return ERR; /* no delete */
+    }
+    
+
+    //** 3 case *//
+    //TODO
+    while (curr_item != NULL) /* finding */
+    {
+        if (strcmp(v, curr_item->val) == 0)
+        {
+            temp = curr_item;
+            curr_item = curr_item->next;
+            free(temp);
+            table->cell[key] = curr_item;
+            return OK;
+        }
+
+        curr_item = curr_item->next;
+    }
+
+    //prev_item->next = curr_item->next;
+    
+    return 0;
+}
+
+/**
+ * @brief Free all data
+ *
+ * @param table - ptr to table
+ */
+void freeTable(sHashTable *table)
+{
+    Item *curr_item = NULL;
+    Item *next_item = NULL;
+
+    for (unsigned int i = 0; i < HASH_SIZE; i++)
+    {
+        curr_item = table->cell[i];
+        while (curr_item != NULL)
+        {
+            next_item = curr_item->next;
+            /* erase data*/
+            memset(curr_item->val, 0x00, strlen(curr_item->val) + 1);
+            curr_item->key = 0;
+            free(curr_item->val);
+            free(curr_item);
+            curr_item = next_item;
+        }
+    }
+
+    free(table->cell);
+}
+
+/**
+ * @brief User function error
+ *
+ */
 void error_alloc_exit(void)
 {
     printf("Error alloc memory\n");
-    // free
     exit(1);
 }
---
+++ hash_table/hash.h
@@ -0,0 +1,53 @@
+#ifndef _HASH_H
+#define _HASH_H
+
+#include <stdint.h>
+#include <string.h>
+
+#define OK  (0)
+#define ERR (-1)
+
+// use prime number
+#define PRIME (176021)
+// #define TEST_MALLOC  (100000000)
+#define HASH_SIZE (PRIME)
+
+/* Hash data */
+struct sHashItem
+{
+    int key;                // hash key    char * val;
+    char *val;              // C-style string
+    struct sHashItem *next; // Using a linked list to resolve collisions
+};
+
+typedef struct sHashItem Item;
+
+/* Hash table */
+typedef struct
+{
+    Item **cell; // array pointer
+    size_t cnt;  //
+    size_t size; // capacity
+} sHashTable;
+
+/* example
+
+    HashTable.cnt;
+    HashTable.size;
+    HashTable.cell[0].key;
+    HashTable.cell[0].val;
+    HashTable.cell[0].next->key;
+
+    HashTable.cell[1].val;
+*/
+
+int hashFunc(const char *val);
+void initHashTable(sHashTable *table);
+void freeTable(sHashTable *table);
+void insertItem(sHashTable *table, const char *data);
+Item *createItem(sHashTable *table, const char *data, const int key);
+Item *findItemVal(sHashTable *table, const char *v);
+int rmItem(sHashTable *table, const char *v);
+void error_alloc_exit(void);
+
+#endif
