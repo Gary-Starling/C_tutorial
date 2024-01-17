af45bdc3c95dc9f020771920a1a517bd16586446
Gary-Starling <skvorec92@mail.ru>
2024-01-17 16:57:27 +0300

    found error in merge_sort(swap array)

--- algorithms/merge_sort/main.c
+++ algorithms/merge_sort/main.c
@@ -27,7 +27,7 @@ int main(int argc, char const *argv[])
 
     srand(time(NULL));
 
-   // arrOut = (data *)malloc(sizeof(data) * _SIZE_OUT);
+    // arrOut = (data *)malloc(sizeof(data) * _SIZE_OUT);
 
     for (uint64_t i = 0; i < len; i++)
         arrIn[i] = rand() % len;
@@ -44,7 +44,7 @@ int main(int argc, char const *argv[])
     }
 
     assert(_SIZE_IN == _SIZE_OUT);
-    mergeSort(arrIn, arrOut, 0, _SIZE_IN);
+    mergeSort(arrIn, arrOut, 0, _SIZE_IN - 1);
 
 #if _PRINT_OUT_ == _YES_
     printf("\nSorted\n");
@@ -62,17 +62,21 @@ int main(int argc, char const *argv[])
  * @param start
  * @param end
  */
+//                          {4, 2, 1, 3}
+//                 ({4, 2}                  {1, 3} )    s = 0; e = 4; mid = 2
+//          ({0 , 2})
+//      (0)        (2)
 void mergeSort(data *arrIn, data *arrOut, uint64_t start, uint64_t end)
 {
     uint64_t mid = (start + end) / 2;
 
     printf("start = %d, end = %d\n", start, end);
 
-    if ((end - start) <= 1)
+    if ((end - start) < 1)
         return;
 
     mergeSort(&arrIn[start], &arrOut[start], start, mid); // left
-    mergeSort(&arrIn[mid], &arrOut[mid], mid + 1, end);   // right
+    mergeSort(&arrIn[mid], &arrOut[mid], mid + 1, end);   // right // arrPrint(arrOut, _SIZE_IN)
     merge(arrIn, arrOut, start, end);
 }
 
@@ -93,8 +97,8 @@ void merge(data *arrIn, data *arrOut, uint64_t start, uint64_t end)
     l1 = start;
     l2 = mid + 1;
     i = start;
-
-    while ((l1 <= mid) && (l2 <= end))
+    //TODO: error, swap array
+    while ((l1 <= mid) && (l2 <= end)) // 1 3 0 3
     {
         if (arrIn[l1] <= arrIn[l2])
             arrOut[i++] = arrIn[l1++];
