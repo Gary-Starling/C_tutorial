b1e9a302134a6c9c2f794125f874dcfbf0d06059
Gary-Starling <skvorec92@mail.ru>
2024-02-12 16:59:54 +0300

    merge time test+

--- algorithms/merge_sort/main.c
+++ algorithms/merge_sort/main.c
@@ -1,55 +1,57 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <assert.h>
 #include <time.h>
 #include <stdlib.h>
 
-#define _SIZE_IN (100)
-#define _SIZE_OUT (100)
 typedef int data;
 
-#define _PRINT_OUT_ _YES_
+#define _PRINT_OUT_ _NO_
 #define _NO_ 0
 #define _YES_ 1
-// #define N (102400U)
+#define N (102400U)
 
-data ArrIn[_SIZE_IN] = {3, 2, 8, 1};
-data ArrOut[_SIZE_IN];
+//result on my PC Sorting time 0.0160 sec
 
-void mergeSort(data *arrIn, data *arrOut, uint64_t start, uint64_t end);
-void merge(data *arrIn, data *arrOut, uint64_t start, uint64_t end);
+void mergeSort(data *arrIn, data *arrOut, unsigned int start, unsigned int end);
+void merge(data *arrIn, data *arrOut, unsigned int start, unsigned int end);
 void arrPrint(data *arr, unsigned int len);
 
 int main(int argc, char const *argv[])
 {
-    uint64_t len = (sizeof(ArrIn) / sizeof(data));
+    data arrIn[N];
+    data *arrOut = NULL;
+    double st, end;
+    unsigned int len = (sizeof(arrIn) / sizeof(data));
 
     srand(time(NULL));
 
-    // arrOut = (data *)malloc(sizeof(data) * _SIZE_OUT);
+    arrOut = (data *)malloc(sizeof(data) * N);
 
-    for (uint64_t i = 0; i < len; i++)
-        ArrIn[i] = rand() % (len + 99);
+    for (unsigned int i = 0; i < len; i++)
+        arrIn[i] = rand() % (len + 99);
 
 #if _PRINT_OUT_ == _YES_
     printf("\nUnsorted\n");
-    arrPrint(ArrIn, len);
+    arrPrint(arrIn, len);
 #endif
 
-    // if (arrOut == NULL)
-    // {
-    //     printf("Memory allocation is failed\n");
-    //     exit(EXIT_FAILURE);
-    // }
+    if (arrOut == NULL)
+    {
+        printf("Memory allocation is failed\n");
+        exit(EXIT_FAILURE);
+    }
 
-    assert(_SIZE_IN == _SIZE_OUT);
-    mergeSort(ArrIn, ArrOut, 0, _SIZE_IN - 1);
+    st = clock();
+    mergeSort(arrIn, arrOut, 0, N - 1);
+    end = clock();
 
 #if _PRINT_OUT_ == _YES_
     printf("\nSorted\n");
-    arrPrint(ArrOut, len);
+    arrPrint(arrOut, len);
 #endif
+
+    printf("Sorting time %.4lf sec.\n", (double)((end - st) / CLK_TCK));
 
     return 0;
 }
@@ -63,20 +65,21 @@ int main(int argc, char const *argv[])
  * @param end
  */
 //                          {4, 2, 1, 3}
-//                 ({4, 2}                  {1, 3} )    
+//                 ({4, 2}                  {1, 3} )
 //              ({4}       {2})          {1}      {3})
-//                      /                       
+//                      /
 //                 ({2, 4})                 ({1,3})     //1,2,3,4
 //                          {1, 2, 3, 4}
-void mergeSort(data *arrIn, data *arrOut, uint64_t start, uint64_t end)
+void mergeSort(data *arrIn, data *arrOut, unsigned int start, unsigned int end)
 {
-    uint64_t mid = (start + end) / 2;
+    unsigned int mid = (start + end) / 2;
 
-    if ((end - start) < 1) return;
+    if ((end - start) < 1)
+        return;
 
     mergeSort(arrIn, arrOut, start, mid);   // left
     mergeSort(arrIn, arrOut, mid + 1, end); // right
-    merge(arrIn, arrOut, start, end);        
+    merge(arrIn, arrOut, start, end);
 }
 
 /**
@@ -87,11 +90,11 @@ void mergeSort(data *arrIn, data *arrOut, uint64_t start, uint64_t end)
  * @param start
  * @param end
  */
-void merge(data *arrIn, data *arrOut, uint64_t start, uint64_t end)
+void merge(data *arrIn, data *arrOut, unsigned int start, unsigned int end)
 {
-    uint64_t l1, l2;                  // left index each part of array a1[0..2] mid a2[3..6]
-    uint64_t mid = (start + end) / 2; // mid element(end of left array and start of right array)
-    uint64_t i;                       // index of out array
+    unsigned int l1, l2;                  // left index each part of array a1[0..2] mid a2[3..6]
+    unsigned int mid = (start + end) / 2; // mid element(end of left array and start of right array)
+    unsigned int i;                       // index of out array
 
     l1 = start;
     l2 = mid + 1;
